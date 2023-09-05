9e61cdd08613d38ebca75bcc2b97507ff6cbaf01
bhuvanesh-sankar <bhuvanesh.sankar@freshworks.com>
2023-09-05 12:03:53 +0530

    FD-120307 :: modified comments

--- lib/lhm/trigger_switcher.rb
+++ lib/lhm/trigger_switcher.rb
@@ -14,8 +14,8 @@ module Lhm
       source_table.triggers.each do |trigger|
         trigger_name = trigger[0]
         trigger_definition = fetch_trigger_definition(trigger_name, connection)
-        # since source table is renamed to lhmn_table_name, triggers associated with the source table will now be associated with lhmn_table_name.
-        # skip if the trigger isn't associated with lhmn_table_name.
+        # since source table is renamed to lhma_table_name(destination table name) after migration, triggers associated with the source table will now be associated with lhma_table_name.
+        # skip if the trigger isn't associated with lhma_table_name.
         next unless trigger_definition.include? destination_table_name
 
         modified_definition = trigger_definition.gsub(destination_table_name, source_table.name)
@@ -26,7 +26,7 @@ module Lhm
 
     def fetch_trigger_definition(trigger_name, connection)
       result = connection.execute("SHOW CREATE TRIGGER #{trigger_name};").first
-      # ["update_message_old_5", "NO_ENGINE_SUBSTITUTION", "CREATE DEFINER=`root`@`localhost` TRIGGER update_message_old_5 BEFORE INSERT ON `lhma_2023_09_04_14_32_54_810_logs_table` FOR EACH ROW\n    BEGIN\n      IF NEW.message = '1' THEN\n        SET NEW.message = 'true';\n      END IF;\n    END", "utf8", "utf8_general_ci", "utf8_unicode_ci", 2023-09-04 14:32:43 UTC]
+      # sample result => ["update_message_old_5", "NO_ENGINE_SUBSTITUTION", "CREATE DEFINER=`root`@`localhost` TRIGGER update_message_old_5 BEFORE INSERT ON `lhma_2023_09_04_14_32_54_810_logs_table` FOR EACH ROW\n BEGIN\n IF NEW.message = '1' THEN\n SET NEW.message = 'true';\n END IF;\n END", "utf8", "utf8_general_ci", "utf8_unicode_ci", 2023-09-04 14:32:43 UTC]
       # create trigger definition will be stored in the third index in the result, ideally result will always be of length 7 if its executed successfully.
       result && result.length > 2 ? result[2] : ''
     end
