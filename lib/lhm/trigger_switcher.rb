30ed98e56819a333fddc01300398a381489e606c
bhuvanesh-sankar <bhuvanesh.sankar@freshworks.com>
2025-10-21 18:46:17 +0530

    Initial commit with dev fix

--- lib/lhm/trigger_switcher.rb
+++ lib/lhm/trigger_switcher.rb
@@ -1,5 +1,7 @@
 module Lhm
   class TriggerSwitcher
+    DEFAULT_TRIGGER_DEFINER = 'migration'.freeze
+
     def copy_triggers(source_table, destination_table_name, connection)
       return unless !destination_table_name.empty? && connection.table_exists?(destination_table_name)
 
@@ -24,7 +26,19 @@ module Lhm
       result = connection.execute("SHOW CREATE TRIGGER #{trigger_name};").first
       # sample result => ["update_message_old_5", "NO_ENGINE_SUBSTITUTION", "CREATE DEFINER=`root`@`localhost` TRIGGER update_message_old_5 BEFORE INSERT ON `lhma_2023_09_04_14_32_54_810_logs_table` FOR EACH ROW\n BEGIN\n IF NEW.message = '1' THEN\n SET NEW.message = 'true';\n END IF;\n END", "utf8", "utf8_general_ci", "utf8_unicode_ci", 2023-09-04 14:32:43 UTC]
       # create trigger definition will be stored in the third index in the result, ideally result will always be of length 7 if its executed successfully.
-      result && result.length > 2 ? result[2] : ''
+      trigger_definition = result && result.length > 2 ? result[2] : ''
+
+      # Replace definer if LHM_CHANGE_TRIGGER_DEFINER environment variable is set
+      puts "ENV['LHM_CHANGE_TRIGGER_DEFINER'] - #{ENV['LHM_CHANGE_TRIGGER_DEFINER']}"
+      trigger_definition = update_trigger_definer(trigger_definition) if ENV['LHM_CHANGE_TRIGGER_DEFINER'] && !trigger_definition.empty?
+      trigger_definition
+    end
+
+    def update_trigger_definer(trigger_definition)
+      puts "update_trigger_definer called"
+      definer_user = ENV['LHM_TRIGGER_DEFINER'] || DEFAULT_TRIGGER_DEFINER
+      # Match DEFINER=`user`@`host` pattern and replace with specified definer user@localhost
+      trigger_definition.gsub(/DEFINER=`[^`]+`@`[^`]+`/, "DEFINER=`#{definer_user}`@`localhost`")
     end
 
     def remove_timestamp_from_trigger_name_if_exists(trigger_name)
